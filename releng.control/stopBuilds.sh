6fb30a1b76eaa7cc391b4bd347e3a10bcbe2d45c
asobolev <asobolev>
2008-11-17 17:44:34 +0000

    *fix

--- releng.control/stopBuilds.sh
+++ releng.control/stopBuilds.sh
@@ -10,12 +10,14 @@ fi
 # allow to terminate themselves, if they will
 echo "Attempting normal terminate of all java processes"
 killall ${JAVA_5_HOME}/bin/java
+killall ${JAVA_5_HOME}/jre/bin/java
 #   pause briefly to allow shutdown to finish
 sleep 10s
 
 # but if the do not terminate themselves, we can safely force them down
 echo "forcing termination of any remaing java processes"
 killall -9 ${JAVA_5_HOME}/bin/java
+killall -9 ${JAVA_5_HOME}/jre/bin/java
 #    pause briefly to allow ports to free up, etc.
 sleep 10s
 
