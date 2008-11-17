24b9a94749851e00d9374f84d8ec23540d66859f
asobolev <asobolev>
2008-11-17 13:56:04 +0000

    * remove replace.sh

--- releng.control/replace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SOURCE_PATH=$1
-cd $SOURCE_PATH
-for file in $SOURCE_PATH/*
-do
-  cp $file $file'_'
-  sed 's/:pserver:anonymous@dev.eclipse.org/:ext:kurtov@turbine.xored.local/g' $file'_' > $file
-  rm $file'_'
-done
