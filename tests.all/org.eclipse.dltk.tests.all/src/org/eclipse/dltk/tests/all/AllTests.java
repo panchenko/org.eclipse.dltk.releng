8438f5712df558e289c9bd337d6eab9ee7260335
apanchenk <apanchenk>
2010-04-23 08:12:00 +0000

    add js.core.tests and rearrange tests in 5 groups

--- tests.all/org.eclipse.dltk.tests.all/src/org/eclipse/dltk/tests/all/AllTests.java
+++ tests.all/org.eclipse.dltk.tests.all/src/org/eclipse/dltk/tests/all/AllTests.java
@@ -24,19 +24,47 @@ public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("DLTK tests"); //$NON-NLS-1$
-		// $JUnit-BEGIN$
+		suite.addTest(createCore());
+		suite.addTest(createJavaScript());
+		suite.addTest(createTCL());
+		suite.addTest(createRuby());
+		suite.addTest(createPython());
+		return suite;
+	}
+
+	private static TestSuite createCore() {
+		TestSuite suite = new TestSuite("Core"); //$NON-NLS-1$
 		suite.addTest(org.eclipse.dltk.core.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.debug.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.ui.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.validators.core.tests.AllTests.suite());
+		suite.addTest(org.eclipse.dltk.formatter.tests.AllTests.suite());
+		return suite;
+	}
 
+	private static TestSuite createPython() {
+		TestSuite suite = new TestSuite("Python"); //$NON-NLS-1$
 		suite.addTest(org.eclipse.dltk.python.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.python.ui.tests.AllTests.suite());
+		return suite;
+	}
 
+	private static TestSuite createRuby() {
+		TestSuite suite = new TestSuite("Ruby"); //$NON-NLS-1$
 		suite.addTest(org.eclipse.dltk.ruby.core.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.ruby.debug.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.ruby.ui.tests.AllTests.suite());
+		// load class from the o.e.d.ruby.formatter.tests fragment
+		suite.addTest(loadClass("org.eclipse.dltk.ruby.formatter.tests", //$NON-NLS-1$
+				"org.eclipse.dltk.ruby.formatter.tests.AllTests")); //$NON-NLS-1$
+		// load class from the o.e.d.ruby.testing.tests fragment
+		suite.addTest(loadClass("org.eclipse.dltk.ruby.testing", //$NON-NLS-1$
+				"org.eclipse.dltk.ruby.testing.tests.AllTests")); //$NON-NLS-1$
+		return suite;
+	}
 
+	private static TestSuite createTCL() {
+		TestSuite suite = new TestSuite("TCL"); //$NON-NLS-1$
 		suite.addTest(org.eclipse.dltk.tcl.core.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.tcl.parser.tests.AllTestsRunner.suite());
 		suite.addTest(org.eclipse.dltk.tcl.validators.tests.AllTests.suite());
@@ -45,21 +73,18 @@ public class AllTests {
 		suite.addTest(org.eclipse.dltk.itcl.core.tests.AllTests.suite());
 		suite.addTest(org.eclipse.dltk.tcl.activestatedebugger.tests.AllTests
 				.suite());
-		// load class from the o.e.d.ruby.formatter.tests fragment
-		suite.addTest(loadClass("org.eclipse.dltk.ruby.formatter.tests", //$NON-NLS-1$
-				"org.eclipse.dltk.ruby.formatter.tests.AllTests")); //$NON-NLS-1$
-		// load class from the o.e.d.ruby.testing.tests fragment
-		suite.addTest(loadClass("org.eclipse.dltk.ruby.testing", //$NON-NLS-1$
-				"org.eclipse.dltk.ruby.testing.tests.AllTests")); //$NON-NLS-1$
+		suite.addTest(org.eclipse.dltk.tcl.formatter.tests.AllTests.suite());
+		return suite;
+	}
 
-		suite.addTest(org.eclipse.dltk.formatter.tests.AllTests.suite());
+	private static TestSuite createJavaScript() {
+		TestSuite suite = new TestSuite("JavaScript"); //$NON-NLS-1$
+		suite.addTest(org.eclipse.dltk.javascript.core.tests.AllTests.suite());
 		suite
 				.addTest(org.eclipse.dltk.javascript.parser.tests.AllTests
 						.suite());
 		suite.addTest(org.eclipse.dltk.javascript.formatter.tests.AllTests
 				.suite());
-		suite.addTest(org.eclipse.dltk.tcl.formatter.tests.AllTests.suite());
-		// $JUnit-END$
 		return suite;
 	}
 
